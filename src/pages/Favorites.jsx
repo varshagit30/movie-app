b56f1dfb91b53e90ecee6f99c9065476c46a8bdf
varsha <varshavsl.30@gmail.com>
2025-03-20 01:20:35 +0530

    implemented sorting on favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,19 +1,45 @@
+import { useState } from "react";
 import "../css/Favorites.css";
 import { useFavorites } from "../context/FavoritesContext";
 import MovieCard from "../components/MovieCard";
 
 const Favorites = () => {
   const { favorites } = useFavorites();
+  const [sortOption, setSortOption] = useState("");
+
+  const sortMovies = (favorites, option) => {
+    if (option === "title") {
+      return [...favorites].sort((a, b) => a.Title.localeCompare(b.Title));
+    }
+    if (option === "year") {
+      return [...favorites].sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+    }
+    return favorites;
+  };
+
+  const sortedMovies = sortMovies(favorites, sortOption);
 
   return (
     <div>
       {favorites.length > 0 ? (
         <div className="favorites">
           <h2>Your Favorites</h2>
+          <div className="sort-container">
+            <select
+              value={sortOption}
+              onChange={(e) => setSortOption(e.target.value)}
+              className="sort-select"
+            >
+              <option value="">Sort by</option>
+              <option value="title">Title</option>
+              <option value="year">Year</option>
+            </select>
+          </div>
           <div className="movies-grid">
-            {favorites.map((movie) => (
-              <MovieCard movie={movie} key={movie.imdbID} />
-            ))}
+            {favorites &&
+              sortedMovies.map((movie) => (
+                <MovieCard movie={movie} key={movie.imdbID} />
+              ))}
           </div>
         </div>
       ) : (
